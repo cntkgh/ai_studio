b692174f539598fdf3c24104d47a7e63caf02154
cntkgh <cnt.kgh@gmail.com>
2022-11-02 17:33:51 +0900

    commit test

--- ai_studio_project/ai_studio_project/settings.py
+++ ai_studio_project/ai_studio_project/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-i1+2il4gu2!er$rhew4z9yt6a69)4jp6=9uo$0vf+$hp9dirbs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# TEST
+
+# SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
